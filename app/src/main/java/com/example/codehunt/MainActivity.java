06f50f32aa9cdb41071f1508b130c6720c6da41d
Skg <shubhankar.gupto.11@gmail.com>
2019-11-03 11:01:39 +0530

    Added Splash Screen. Very basic.

--- app/src/main/java/com/example/codehunt/MainActivity.java
+++ app/src/main/java/com/example/codehunt/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.codehunt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIMEOUT = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIMEOUT);
+
+        Toast.makeText(this, "Please make sure you have a stable internet connection!", Toast.LENGTH_LONG).show();
     }
 }
