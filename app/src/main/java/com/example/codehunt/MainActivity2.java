2cc22422b467692e06ea890c0c69d040bada711c
Skg <shubhankar.gupto.11@gmail.com>
2019-11-05 21:31:23 +0530

    The app runs!

--- app/src/main/java/com/example/codehunt/MainActivity2.java
+++ app/src/main/java/com/example/codehunt/MainActivity2.java
@@ -17,7 +17,7 @@ public class MainActivity2 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
 
         mSectionsPageAdapter = new SectionsPageAdapter(getSupportFragmentManager());
 
